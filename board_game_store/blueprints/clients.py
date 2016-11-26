e58de1b7c21509798e61c0f59033fe64e36a2bf6
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2016-11-26 17:34:45 -0200

    Show error messsages on INSERT operation

--- board_game_store/blueprints/clients.py
+++ board_game_store/blueprints/clients.py
@@ -17,11 +17,16 @@ class AddClientForm(FlaskForm):
 
 @clients_blueprint.route('/clients/add-client', methods=['GET', 'POST'])
 def add_client_page():
+    def error(message):
+        return redirect('/error', message=message, page='clients/add_client.html')
     form = AddClientForm()
     if form.validate_on_submit():
         cpf = form.cpf.data
         if len(cpf) != 11 or any(x < '0' or x > '9' for x in cpf):
-            return redirect('/error', message='CPF deve possuir 11 dígitos numéricos', page='clients/add_client.html')
-        add_user(form.cpf.data, form.name.data, form.surname.data, form.birthday.data)
+            return error('CPF deve possuir 11 dígitos numéricos')
+        try:
+            add_user(form.cpf.data, form.name.data, form.surname.data, form.birthday.data)
+        except Exception as e:
+            return error('Erro no banco de dados: {}'.format(e))
         return redirect('success')
     return render_template('clients/add_client.html', form=form)
