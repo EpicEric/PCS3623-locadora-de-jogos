fb6aebdf126dfdd98c94b3aabef810647ae12098
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2016-11-25 22:43:10 -0200

    Map root page to main_menu.html, move files to templates/ and static/

--- board_game_store/app.py
+++ board_game_store/app.py
@@ -1,10 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 import dao
-
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def get_client_in_room():
-    return repr(dao.get_room_reservation())
+def root_page():
+    return render_template('main_menu.html')
