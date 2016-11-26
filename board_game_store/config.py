e0c500dee3c439c465018ecd51b60bf67f727879
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2016-11-26 17:49:44 -0200

    Get config keys from environment variables

--- board_game_store/config.py
+++ board_game_store/config.py
@@ -6,9 +6,16 @@ _config = None
 
 def load(app):
     global _config
-    with open(os.path.join(app.root_path, 'config.json')) as f:
-        _config = json.load(f)
+    path = os.path.join(app.root_path, 'config.json')
+    if os.path.isfile(path):
+        with open(path) as f:
+            _config = json.load(f)
 
 
 def get(key):
-    return _config[key]
+    if _config and key in _config:
+        return _config[key]
+    value = os.environ.get('BOARD_STORE_CONFIG_{}'.format(key))
+    if value is not None:
+        return value
+    raise RuntimeError('Config key not found: {}'.format(key))
