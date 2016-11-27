6d08efc4c45b0f07536fc1feb02a3f9e46c9799b
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2016-11-26 23:14:51 -0200

    Employee registration, force HTML field validation

--- board_game_store/app.py
+++ board_game_store/app.py
@@ -1,4 +1,5 @@
 from .blueprints.clients import clients_blueprint
+from .blueprints.employees import employees_blueprint
 from .blueprints.games import games_blueprint
 from .blueprints.rooms import rooms_blueprint
 import board_game_store.config as config
@@ -8,6 +9,7 @@ app = Flask(__name__)
 config.load(app)
 app.secret_key = config.get('secret-key')
 app.register_blueprint(clients_blueprint)
+app.register_blueprint(employees_blueprint)
 app.register_blueprint(games_blueprint)
 app.register_blueprint(rooms_blueprint)
 
