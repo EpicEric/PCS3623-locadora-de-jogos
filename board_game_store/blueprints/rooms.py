8475be0b5f8d393725c6b02f9d115095c7a335ce
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2016-11-27 18:57:15 -0200

    Room reservation form

--- board_game_store/blueprints/rooms.py
+++ board_game_store/blueprints/rooms.py
@@ -1,8 +1,8 @@
-from board_game_store.db.access import add_room, get_all_room_numbers, get_free_rooms
+from board_game_store.db.access import add_reservation, add_room, get_all_room_numbers, get_free_rooms
 from datetime import datetime
-from flask import Blueprint, flash, redirect, render_template
+from flask import Blueprint, flash, redirect, render_template, request
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, StringField
+from wtforms import IntegerField, SelectField, StringField
 from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired, NumberRange
 
@@ -16,8 +16,12 @@ class AddRoomForm(FlaskForm):
 
 class ViewRoomForm(FlaskForm):
     seats = IntegerField('Número de pessoas', validators=[DataRequired(), NumberRange(min=1, max=30)])
-    date = DateField('Dia')
-    time = StringField('Horário')
+    date = DateField('Dia', validators=[DataRequired()])
+    time = StringField('Horário', validators=[DataRequired()])
+    room = SelectField('Salas disponíveis')
+    end_date = DateField('Data de fim')
+    end_time = StringField('Horário de fim')
+    client_cpf = StringField('CPF do cliente')
 
 
 @rooms_blueprint.route('/rooms/add-room', methods=['GET', 'POST'])
@@ -49,20 +53,28 @@ def list_rooms_page():
     return render_template('rooms/list_rooms.html', room_list=room_list)
 
 
-@rooms_blueprint.route('/rooms/view-room', methods=['GET', 'POST'])
-def view_rooms_page():
+@rooms_blueprint.route('/rooms/reserve-room', methods=['GET', 'POST'])
+def reserve_room_page():
     def error(message):
         flash(message)
         return redirect('/error')
     form = ViewRoomForm()
-    if form.validate_on_submit():
-        try:
-            desired_time = '{} {}'.format(form.date.data, form.time.data)
-            return render_template(
-                'rooms/view_room.html',
-                rooms=get_free_rooms(form.seats.data, datetime.strptime(desired_time, '%Y-%m-%d %H:%M')))
-        except Exception as e:
-            import traceback
-            return error('Erro no banco de dados: {}'.format(traceback.format_exc()))
-        return redirect('success')
-    return render_template('rooms/view_room.html', form=form)
+    if request.method == 'POST':
+        desired_time = datetime.strptime('{} {}'.format(form.date.data, form.time.data), '%Y-%m-%d %H:%M')
+        if form.room.data != 'None':
+            end_time = datetime.strptime('{} {}'.format(form.end_date.data, form.end_time.data), '%Y-%m-%d %H:%M')
+            if end_time < desired_time:
+                return error('Tempo de fim deve ser maior que tempo de início')
+            add_reservation(form.room.data, desired_time, end_time, form.client_cpf.data, None)
+            return redirect('success')
+        else:
+            try:
+                form.room.choices = [
+                    (x[0], '{} - {}'.format(x[0], x[1]))
+                    for x in get_free_rooms(form.seats.data, desired_time)
+                ]
+                return render_template('rooms/reserve_room.html', form=form)
+            except Exception as e:
+                import traceback
+                return error('Erro no banco de dados: {}'.format(traceback.format_exc()))
+    return render_template('rooms/reserve_room.html', form=form)
